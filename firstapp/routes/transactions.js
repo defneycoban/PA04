0dd9645f49ad00072dfcf035a2f60b7162ff66e6
defneycoban <dcoban@brandeis.edu>
2023-04-24 00:38:56 -0400

    updated ejs file

--- firstapp/routes/transactions.js
+++ firstapp/routes/transactions.js
@@ -12,32 +12,110 @@ isLoggedIn = (req,res,next) => {
   }
 }
 
-// Get all transactions
-router.get('/transactions', async function(req, res, next) {
-  try {
-    const transactions = await Transaction.find({});
-    res.render('transactions', { transactions });
-  } catch (err) {
-    next(err);
-  }
+// Normalize the formatting of dates
+function normalizeDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+//Get all transcations
+router.get('/transactions/', isLoggedIn, async (req, res, next) => {
+        const sortColumn = req.query.sortBy || "date";
+        let data;
+
+    switch (sortColumn) {
+        case "description":
+            data = {
+            description: 1,
+            date: -1,
+        };
+        break;
+        case "amount":
+            data = {
+            amount: 1,
+            date: -1,
+        };
+        break;
+        case "category":
+            data = {
+            category: 1,
+            date: -1,
+        };
+        break;
+        default:
+            data = {
+            date: -1,
+            description: 1,
+        };
+        break;
+    }
+
+  let items = await Transactions.find({ userId: req.user._id })
+    .sort(data)
+    .collation({ locale: "en", strength: 2 });
+
+  res.render("transactionslist", { items });
 });
 
 // Create a new transaction
-router.post('/transactions', async function(req, res, next) {
-  try {
-    const newTransaction = new Transaction(req.body);
-    await newTransaction.save();
-    res.redirect('/transactions');
-  } catch (err) {
-    next(err);
-  }
+router.post("/transaction", isLoggedIn, async (req, res, next) => {
+        const t = new Transaction(
+            {description: req.body.description,
+            amount: req.body.amount,
+            category: req.body.category,
+            date: req.body.date,
+            userId: req.user._id,
+        });
+        await t.save();
+        res.redirect("/transactions");
 });
 
+//Delete a transaction
 router.get('/transactions/delete/:itemID', isLoggedIn, async (req, res, next) => {
   await Transaction.deleteOne({_id:req.params.itemID});
   res.redirect('/transactions');
 });
 
+//Edit a transaction
+router.get("/transactions/edit/:itemId", isLoggedIn, async (req, res, next) => {
+    console.log("inside /transactions/edit/:itemId");
+    const tr = await Transaction.findById(req.params.itemId);
+    res.locals.item = tr;
+    res.render("edit", { normalizeDate });
+});
+
+//Update a transaction
+router.post("/transactions/updateTransaction", isLoggedIn, async (req, res, next) => {
+    const { itemId, description, amount, category, date } = req.body;
+    //console.log("inside /transactions/updateTransaction");
+    await Transaction.findOneAndUpdate(
+      { _id: itemId },
+      { $set: { description, amount, category, date } }
+    );
+    res.redirect("/transactions");
+  }
+);
+
+router.get('/transaction/groupByCategory',
+  isLoggedIn,
+  async (req, res, next) => {
+    console.log("inside /transaction/groupByCategory")
+    const userId = req.user._id
+      let results =
+            await transactionItem.aggregate(
+                [ 
+                  {$match:{
+                    userId: userId}},
+                  {$group:{
+                    _id:'$category',
+                    total:{$sum:1}
+                    }},
+                  {$sort:{total:-1}},              
+                ])
+        res.render('groupByCategory', {results})
+      });
 
 // Delete a transaction
 // router.delete('/transactions/:id', isLoggedIn, async (req, res, next) => {
